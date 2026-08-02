4ac82df3f47a530b437c60fa0a825b63a201e3ef
Alex Zarnitsa <alex@zarnitsa.com>
2026-08-02 10:20:19 -0400

    Added process image task

--- backend/dispatcher/workers/config.py
+++ backend/dispatcher/workers/config.py
@@ -2,7 +2,7 @@ from arq.connections import RedisSettings
 from arq.worker import func
 
 from dispatcher.core import settings
-from dispatcher.workers.tasks import process_bookmark_migrate
+from dispatcher.workers.tasks import process_bookmark_image
 
 from .remove_bookmark import process_bookmark_remove
 from .save_bookmark import process_bookmark_save
@@ -12,7 +12,7 @@ class WorkerSettings:
     functions = [
         func(process_bookmark_save, name="process_bookmark:save"),
         func(process_bookmark_remove, name="process_bookmark:remove"),
-        func(process_bookmark_migrate, name="process_bookmark:migrate", timeout=18000),
+        func(process_bookmark_image, name="process_bookmark:migrate", timeout=18000),
     ]
     redis_settings = RedisSettings(
         host=settings.redis.redis_host, port=settings.redis.redis_port
